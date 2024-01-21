9416156b364674a1aa1874065662bdf2178d6018
Bryce Kormylo <50851779+ATxLS-dev@users.noreply.github.com>
2024-01-21 14:14:53 -0800

    Layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,11 +4,11 @@ import Dogs from "./(components)/dogs";
 
 export default function About() {
   return (
-    <div className="flex min-h-[200vh] w-screen flex-col gap-[10rem] pt-10">
+    <div className="w-screen flex flex-col gap-[10rem] pt-10">
       <Ellie />
-      <div className="divider self-end"/>
+      <div className="divider self-end" />
       <Bryce />
-      <div className="divider self-start"/>
+      <div className="divider self-start" />
       <Dogs />
     </div>
   );
