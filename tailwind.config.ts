4ef735e7319a226babacdd80f33b794b5640a8a2
Bryce Kormylo <50851779+ATxLS-dev@users.noreply.github.com>
2024-01-21 16:49:41 -0800

    shadows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
         'dark': '0px 0px 8px 0px rgba(0, 0, 0, 0.7)',
         'inset': 'inset 0rem 0.2rem 0.4rem 0 rgba(0,0,0,0.1)',
         'neo': '0.625rem 0.625rem 0.875rem 0 rgba(161,159,153,0.2), -0.5rem -0.5rem 1.125rem 0 rgba(255,255,255, 0.3)',
-        'neodark': '0.25rem 0.25rem 0.5rem 0 rgba(26,23,21), -0.25rem -0.25rem 0.5rem 0 rgba(52,47,41)',
+        'neodark': '0.25rem 0.25rem 0.5rem 0 rgba(26,23,21,0.8), -0.25rem -0.25rem 0.5rem 0 rgba(52,47,41,0.4)',
       },
     },
     colors: {
